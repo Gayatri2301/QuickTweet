1cc0846ca7a24538ef8a8801ad1ffbd6e83cb3ca
Gayatri2301 <gayathriedibilli@gmail.com>
2024-08-12 22:35:55 +0530

    dropdown

--- frontend/quick-tweet/src/App.jsx
+++ frontend/quick-tweet/src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './Components/Login'
 
 
 const App = () => {
 
+  const [isOpen, setIsOpen] = useState(false);
+  const options = ['Profile','Settings','Logout']
 
   return (
     <div className='h-40'>
       <p className='text-white font-bold h-12'><img className='h-12 block inline' src='logo.jpg'></img> QuickTweet </p>
       <div>
-      <img className='absolute top-0 right-0 h-14 w-14 mr-10 mt-12 border rounded-full cursor-pointer' src='profile.jpg'></img>
+      <button onClick={() => setIsOpen(!isOpen)}><img className='absolute top-0 right-0 h-14 w-14 mr-10 mt-12 border rounded-full cursor-pointer' src='profile.jpg'></img></button>
       </div>
+      {isOpen &&
       <div className='border rounded bg-white absolute top-[110px] right-5 w-24'>
-        <ul>
-          <li className='p-2 mb-1 h-8 text-lg cursor-pointer hover:bg-gray-300'>Profile</li>
-          <li className='p-2 mb-1 h-8 text-lg cursor-pointer hover:bg-gray-300'>Settings</li><hr></hr>
-          <li className='p-2 mb-1 h-8 text-lg cursor-pointer hover:bg-gray-300'>Logout</li>
-        </ul>
+        {options && options.map((option) => {
+          return <div className='border-b px-4 py-2 hover:bg-gray-300'>{option}</div>
+        })}
       </div>
+    }
     </div>
   )
 }
